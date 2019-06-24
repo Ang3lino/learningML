84fa8d179cb270ba927111cb780ad18bc3a62079
Angel Lopez Manriquez <sigma271@outlook.com>
2019-06-23 22:08:38 -0500

    Add files via upload

--- regressions/svr/svr.py
+++ regressions/svr/svr.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,9 +6,12 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
+import os
+wd = os.getcwd()
+
 # load the dataframe
-df = pd.read_csv("Position_Salaries.csv")
-X = df.iloc[:, 1:2] # all rows, from 1 until 2 (2 is exclusive)
+df = pd.read_csv(os.path.join('svr', 'Position_Salaries.csv'))
+X = df.iloc[:, 1:2]  # all rows, from 1 until 2 (2 is exclusive)
 y = df.iloc[:, 2:3]
 
 # Split dataset in training, testing n dimensional arrays, if required
@@ -35,8 +36,8 @@ y_pred = sc_y.inverse_transform(regression.predict(sc_X.transform(np.array([[6.5
 # data display
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape(len(X_grid), 1)
-plt.scatter(X, y, color = "red")
-plt.plot(X_grid, regression.predict(X_grid), color = "blue")
+plt.scatter(X, y, color="red")
+plt.plot(X_grid, regression.predict(X_grid), color="blue")
 plt.title("Modelo de Regresión (SVR)")
 plt.xlabel("Posición del empleado")
 plt.ylabel("Sueldo (en $)")
